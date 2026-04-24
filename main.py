f5c42696b5263f32e89c1d563622af8279d8eb68
koushikpotnuru71-creator <koushikpotnuru71@gmail.com>
2026-04-24 18:45:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import json
+from utils import greet_user
 
+with open("data.json") as f:
+    data = json.load(f)
+
+greet_user(data["name"])
+
+print("Skills:")
+for skill in data["skills"]:
+    print("-", skill)
